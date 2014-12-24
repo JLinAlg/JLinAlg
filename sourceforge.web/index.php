010c0a7b0ca37b78db8d30405adf2aafc620304c
Andreas Keilhauer <13208438+keilhauer@users.noreply.github.com>
2014-12-24 00:11:52 +0000

    HilbertMatrixDemo für Webseite vorbereitet.

--- sourceforge.web/index.php
+++ sourceforge.web/index.php
@@ -88,7 +88,7 @@
     Gaussian (normally distributed) noise.</li>
   <li>Application of user-defined functions to all elements of a matrix 
 	 or vector (or pairs of matrices and vectors).</li>
-  <li>Optimisation of mixed-type parameter functions using a gradient search. Example is given.</li> 
+
 </ul>
 
 <a name="GettingStarted"></a>
@@ -99,7 +99,7 @@
 <li>Go to the <a href="#Download">Download</a> section and get the latest version of JLinAlg.</li>
 <li>Set up your environment (We recommend to use <a href="http://www.eclipse.org">Eclipse</a> in any of the following two cases):
    <ul>
-      <li>If you want to use JLinAlg as a library, use jlinalg-0.xx.jar contained in the Zip-file. 
+      <li>If you want to use JLinAlg as a library, use jlinalg-v0.xx.jar contained in the Zip-file. 
           In Eclipse you can just add it to your project by going to Project -> Properties -> 
           java build path -> libraries. There you can use "Add Jars" (drag and drop jlinalg-0.xx.jar 
           to your project first) or "Add External Jars" and select the location of jlinalg-0.xx.jar</li>
@@ -112,7 +112,6 @@
    </ul>
 </li>
 <li>Get to know JLinAlg. Have a look at the <a href="#Examples">examples</a> and the <a href="#Documentation">documentation</a> (both also included in the Zip-File).</li>
-<li>Consider the <a href="structure.html">structure</a> of JLinalg.</li> 
 <li>Do something cool with it.</li>
 <li>Use our <a href="https://sourceforge.net/forum/?group_id=71387">forums</a> at Sourceforge and tell us about it!</li>
 </ol>
@@ -121,8 +120,8 @@
  
 <h2>Download</h2>
 <ul>
-  <li><a href="http://downloads.sourceforge.net/jlinalg/jlinalg_0.6.zip?use_mirror=">JLinAlg-0.6</a>: 
-    The complete source code including Eclipse project files, the API as a jar-file (compiled using jdk1.6.0_16-b01) and javadoc in one zip-file.</li>
+  <li><a href="http://sourceforge.net/projects/jlinalg/files/jlinalg_0.6.zip/download">JLinAlg-v0.6</a>: 
+    The complete source code including Eclipse project files, the API as a jar-file and javadoc in one zip-file.</li>
 	
   <li><a href="https://sourceforge.net/project/showfiles.php?group_id=71387" target="_blank">Earlier 
     versions</a>: Other releases at Sourceforge.</li>
@@ -132,48 +131,42 @@
  
 <h2>Documentation</h2>
 <ul>
-  	<li><a href="/javadoc" target="_blank">JLinalg API-Documentation</a> (generated Javadoc): Quite 
+  	<li><a href="/javadocs" target="_blank">JLinalg API-Documentation</a> (generated Javadoc): Quite 
     thorough documentation of every method and constructor.</li>
-  	<li> JLinAlg uses SVN: <a href="https://jlinalg.svn.sourceforge.net/svnroot/jlinalg">https://jlinalg.svn.sourceforge.net/svnroot/jlinalg  </a> (older versions prior and inclusive to 0.5 are under  <a href="http://jlinalg.cvs.sourceforge.net/jlinalg/" target="_blank">CVS</a>)</li>
+  	<li>Browse the history of our <a href="http://sourceforge.net/p/jlinalg/code/HEAD/tree/">SVN repository</a> or, even further back, the one in our old <a href="http://jlinalg.cvs.sourceforge.net/jlinalg/" target="_blank">CVS repository</a>.</li>
 </ul>
 <a name="Examples"></a>
  
  
 <h2>Examples</h2>
 <ul>
-<LI> <li>The class F2 represents an element of the modulo 2 field F2 and a factory as inner class. 
+  <li>The class F2 represents an element of the modulo 2 field F2 and a factory as inner class. 
    F2 is a field,
    just like the rational numbers. The examples in  <a href="F2Demo.html">F2Demo.java</a> illustrate how to create elements in F2 using the 
-    factory, how to create matrices, and to operate on them  (<a type="text/plain" href="F2Demo.java">source</a>).  The file <a href="F2.html">F2.java</a>  shows how to implement a type and a factory.
-      (<a type="text/plain" href="F2.java">source</a>). </li>
+    factory, how to create matrices, and to operate on them. The file <a href="F2.html">F2.java</a> shows how to implement a type and a factory.</li>
   <li><a href="LinearEquationSystemDemo.html">LinearEquationSystemDemo.java</a>: 
   	This example shows how to 
-    calculate a solution or its solution space  (<a type="text/plain"
-     href="LinearEquationSystemDemo.java">source</a>).</li>
+    calculate a solution or its solution space.</li>
   <li><a href="MatrixOperationsDemo.html">MatrixOperationsDemo.java</a>: How to calculate them for 
-    matrices over arbitrary fields  (<a type="text/plain" href="MatrixOperationsDemo.java">source</a>).</li>
+    matrices over arbitrary fields.</li>
   <li><a href="EigenvaluesDemo.html">EigenvaluesDemo.java</a>: This example 
     shows how JLinAlg can do eigenvalue computation 
-    with real matrices (matrices with DoubleWrapper entries) 
-    (<a type="text/plain" href="EigenvaluesDemo.java">source</a>).</li>
+    with real matrices (matrices with DoubleWrapper entries).</li>
   <li><a href="FieldPDemo.html">FieldPDemo.java</a>: This demonstration of the class FieldP shows that in vector spaces over 
     finite fields there can be linear dependent vectors which are all orthogonal to 
-    each other (<a type="text/plain" href="FieldPDemo.java">source</a>).</li>
-  <li><a href="Xor.html">Xor.java</a>: Exclusive-Or neural net problem using JLinAlg  
-    (<a type="text/plain" href="Xor.java">source</a>).</li>
-    <li><a href="RandomGradientSearchDemo.html">RandomGradientSearchDemo.html.java</a>: search for the smallest residual of a function. 
-    	Needed: <a href="Function.html">Function.html.java</a>
+    each other.</li>
+  <li><a href="Xor.html">Xor.java</a>: Exclusive-Or neural net problem using JLinAlg.</li>
+  <li><a href="HilbertMatrixDemo.java.html">HilbertMatrixDemo.java</a>: This demo shows how the squared distance between the real solution of an equation system to the one that is found when using floating point arithmetic, grows exponentially when the dimension of the Hilbert matrix increases.</li>
 </ul>
 
 <a name="Members"></a>	
 <h2>Project Members</h2>
-<ul><li><a href="http://www.andreaskeilhauer.de" target="_blank">Andreas Keilhauer</a>: Freelance software engineer (founder of JLinAlg), Germany</li>
+<ul><li><a href="http://www.keilhauer.eu" target="_blank">Andreas Keilhauer</a>: Freelance software engineer and founder of JLinAlg, Germany</li>
 	<li><a href="http://www.cs.wlu.edu/~levy" target="_blank">Simon D. Levy</a>: Assoc. Professor, Washington and Lee University, USA</li>
-	<li><a href="http://pp.info.uni-karlsruhe.de/personhp/andreas_lochbihler.php">Andreas Lochbihler</a>: 
-    Research Associate, Universit&auml;t Karlsruhe, Germany</li>
-	<li><a href="mailto:oekmen@in.tum.de">Safak &Ouml;kmen</a>: Informatics student, TU M&uuml;nchen, Germany.
+	<li><a href="http://pp.info.uni-karlsruhe.de/personhp/andreas_lochbihler.php">Andreas Lochbihler</a>: Postdoc at the ETH Zurich, Switzerland</li>
+	<li><a href="mailto:oekmen@in.tum.de">Safak &Ouml;kmen</a>: Computer Scientist, TU Munich, Germany.
 	<li><a href="http://www.adam.ntu.edu.sg/~mgeorg">Georg Lothar Thimm</a>: Asst. Professor, Nanyang Technological University, Singapore</li>
-	<li><a href="http://www.wuerzebesser.de">Christian W&uuml;rzebesser</a>: Research Associate, EAWAG, Z&uuml;rich, Switzerland</li>
+	<li>Christian W&uuml;rzebesser: former Research Associate, EAWAG, Zurich, Switzerland</li>
 	<li>... and perhaps you, too. Use our <a href="https://sourceforge.net/forum/?group_id=71387">forums</a> at Sourceforge and contact us!</li>
 </ul>
 <a name="Links"></a>
@@ -183,7 +176,7 @@
 <ul>
   <li> <a href="https://sourceforge.net/projects/jlinalg/" target="_blank">JLinAlg 
     at Sourceforge</a>: Project website at Sourceforge</li>
-  <li> <a href="http://www.andreaskeilhauer.de/ProPra" target="_blank">Regression 2003</a>: Program for regression analysis (uses JLinAlg)</li>
+  <li> <a href="http://www.keilhauer.eu/ProPra" target="_blank">Regression 2003</a>: Program for regression analysis (uses JLinAlg)</li>
   <li> <a href="http://snarli.sourceforge.net" target="_blank">SNARLI</a>: Java-library 
     for neural networks (uses JLinALg)</li>
     <li><a href="http://www.adam.ntu.edu.sg/~mgeorg/">Crystal Net tools</a> analyse auto- and isomorphisms of infinite nets (representing often crystals ;-) and needs vectors and matrices with rational numbers for this.
@@ -195,10 +188,8 @@
 This project is hosted on...<br>
 <A href="http://sourceforge.net"><IMG src="http://sourceforge.net/sflogo.php?group_id=71387&amp;type=5" alt="SourceForge.net Logo" width="210" height="62" border="0"></A><br><br>
 
-<Strong>Last modification:</Strong> <?php echo(date("d.m.Y", filemtime("index.php"))); 
-echo(", " . date("H:i ", filemtime("index.php"))); 
-?><br>
-by <em>Georg THimm</em><br><br>
+<Strong>Last modification:</Strong> <?php echo(date("d.m.Y, H:i ", filemtime("index.php")));?><br>
+<br>
   <p>
     <a href="http://validator.w3.org/check?uri=referer"><img border="0" src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>
   </p>
