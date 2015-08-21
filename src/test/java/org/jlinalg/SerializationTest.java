d732c59bb4bbf8f1087fe74a2874fea02e533bfc
Andreas Keilhauer <13208438+keilhauer@users.noreply.github.com>
2015-08-21 19:51:59 +0000

    IRingElementFactory does not extend Serializable, because without it Vectors and Matrices cannot be serialized.

---
+++ src/test/java/org/jlinalg/SerializationTest.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of JLinAlg (<http://jlinalg.sourceforge.net/>).
+ * 
+ * JLinAlg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * JLinAlg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JLinALg. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jlinalg;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import org.jlinalg.doublewrapper.DoubleWrapper;
+import org.junit.Test;
+
+public class SerializationTest
+{
+	@Test
+	public void serializeVector() throws IOException
+	{
+		Vector<DoubleWrapper> testVector = new Vector<DoubleWrapper>(
+				new DoubleWrapper[] {
+						new DoubleWrapper(12), new DoubleWrapper(12)
+				});
+		ObjectOutputStream out = new ObjectOutputStream(
+				new ByteArrayOutputStream());
+		out.writeObject(testVector);
+
+	}
+
+}
